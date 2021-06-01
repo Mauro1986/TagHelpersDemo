7917b1624a9ebd87c4372b20b2668e767eaada4a
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2021-06-01 12:08:32 +0200

    Add project files.

---
+++ TagHelpersDemo/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TagHelpersDemo.Models;
+
+namespace TagHelpersDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Product product)
+        {
+
+            product.BillAmount = product.Cost * product.Quantity;
+            if (product.BillAmount > 100 && product.IsPartOfDeal)
+            {
+                product.Discount = product.BillAmount * 10 / 100;
+            }
+            else
+            {
+                product.Discount = product.BillAmount * 5 / 100;
+            }
+
+            ViewBag.BillAmount = product.BillAmount;
+            ViewBag.Discount = product.Discount;
+
+            product.NetBillAmount = product.BillAmount - product.Discount;
+
+
+            switch (product.CategoryID)
+            {
+                case 1:
+                case 2:
+                    product.NetBillAmount -= 10;
+                    ViewBag.NetBillAmount = product.NetBillAmount;
+                    break;
+                default:
+                    break;
+            }
+            return View(product);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TagHelpersDemo/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelpersDemo.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public IEnumerable<Product> Products { get; set; }    //relationship = One To Many   1Category= many Products
+        public List<SelectListItem> Categorys { get; } = new List<SelectListItem>
+        {
+        new SelectListItem {Value = "1",Text="Fashion" },
+        new SelectListItem {Value = "2",Text="Elektronics" },
+        new SelectListItem {Value = "3",Text="Computers" },
+        new SelectListItem {Value = "4",Text="Sports" },
+        };
+    }
+}
